a67b0cdb841cc6efe1e903c7eb7c476b6dec4d01
Aarya Chopkar <aaryachopkar@gmail.com>
2025-01-08 21:46:19 +0530

    feat: create photo

--- src/main/java/com/jetbrains/marco/photoz/clone/PhotozController.java
+++ src/main/java/com/jetbrains/marco/photoz/clone/PhotozController.java
@@ -6,11 +6,15 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -37,10 +41,17 @@ public class PhotozController {
         return photo;
     }
 
-    @DeleteMapping("/photoz")
+    @DeleteMapping("/photoz/{id}")
     public void delete(@PathVariable String id){
         Photo photo =  db.remove(id);
         if(photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/photoz")
+    public ResponseEntity<Photo> create(@RequestBody Photo photo) {
+        photo.setId(UUID.randomUUID().toString());
+        db.put(photo.getId(), photo);
+        return new ResponseEntity<>(photo, HttpStatus.CREATED);
     }
     
 }
